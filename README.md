a627e5df02b404dc80dd9cd7a0951982195b6af4
gangel <grzegorz.aniol@gmail.com>
2018-03-03 20:03:56 +0100

    Change performance units to ms

---
+++ README.md
@@ -0,0 +1,3 @@
+Neo4j
+RESULT PersonTimer   avg=130,437 ms, min=41,471 ms, max=306,532 ms, 
+RESULT RelationTimer avg= 40,747 ms, min= 0,455 ms, max=136,625 ms, 
--- src/main/java/people/performance/MetricsFormatter.java
+++ src/main/java/people/performance/MetricsFormatter.java
@@ -1,7 +1,7 @@
 package people.performance;
 
+import com.codahale.metrics.Snapshot;
 import com.codahale.metrics.Timer;
-import lombok.val;
 
 import java.time.Duration;
 import java.time.temporal.TemporalUnit;
@@ -9,12 +9,12 @@ import java.time.temporal.TemporalUnit;
 public class MetricsFormatter {
 
     public static String getMetricDescription(Timer timer, TemporalUnit unit) {
-        val str = new StringBuffer();
-        val snapshot = timer.getSnapshot();
+        StringBuffer str = new StringBuffer();
+        Snapshot snapshot = timer.getSnapshot();
         long avg = Math.round(snapshot.getMean());
-        str.append("avg=").append(String.format("%.3f", Duration.of(avg, unit).getNano() * 1e-9)).append(", ");
-        str.append("min=").append(String.format("%.3f", Duration.of(snapshot.getMin(), unit).getNano() * 1e-9)).append(", ");
-        str.append("max=").append(String.format("%.3f", Duration.of(snapshot.getMax(), unit).getNano() * 1e-9)).append(", ");
+        str.append("avg=").append(String.format("%.3f ms", Duration.of(avg, unit).getNano() * 1e-6)).append(", ");
+        str.append("min=").append(String.format("%.3f ms", Duration.of(snapshot.getMin(), unit).getNano() * 1e-6)).append(", ");
+        str.append("max=").append(String.format("%.3f ms", Duration.of(snapshot.getMax(), unit).getNano() * 1e-6)).append(", ");
         return str.toString();
     }
 
